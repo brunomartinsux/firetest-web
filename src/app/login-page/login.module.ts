d7f1d914f97d088bb35eaad65abbe9de0e46530a
BrunoAtmanDev <bruno.martins@atmansystems.com>
2021-08-23 21:10:02 -0300

    build: Login Saga - Login, Register n Pass recovery

--- src/app/login-page/login.module.ts
+++ src/app/login-page/login.module.ts
@@ -5,16 +5,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { LoginPageComponent } from './login-page.component';
 import { ComponentsModule } from '../components/components.module';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 
 
  const routes: Routes = [
-   { path: '', component: LoginPageComponent }
+   { path: '', component: LoginPageComponent },
+   { path: 'recover-password', component: RecoverPasswordComponent},
+   { path: 'register', component: RegisterPageComponent}
+
   ]
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    RecoverPasswordComponent,
+    RegisterPageComponent
   ],
   imports: [
     ComponentsModule,
